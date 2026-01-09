78ce49b3ac090a8d251e89ad51085a778af22e82
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-09 18:59:03 +0900

    camera

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,72 @@
-import "./style.css";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
-import typescriptLogo from "./typescript.svg";
+import * as THREE from "three";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const width = 960;
+const height = 540;
+let rot = 0;
+let mouseX = 0;
+document.addEventListener("mousemove", (event) => {
+	mouseX = event.pageX;
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector("#myCanvas")!,
+});
+
+renderer.setSize(width, height);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, width / height);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff);
+directionalLight.position.set(1, 1, 1);
+scene.add(directionalLight);
+
+const texture = new THREE.TextureLoader().load("../public/earth.jpg");
+texture.colorSpace = THREE.SRGBColorSpace;
+
+const material = new THREE.MeshStandardMaterial({
+	map: texture,
+	side: THREE.DoubleSide,
+});
+const geometry = new THREE.SphereGeometry(300, 30, 30);
+const earthMesh = new THREE.Mesh(geometry, material);
+scene.add(earthMesh);
+
+function createStarField() {
+	const vertices = [];
+	for (let i = 0; i < 1000; i++) {
+		const x = 3000 * (Math.random() - 0.5);
+		const y = 3000 * (Math.random() - 0.5);
+		const z = 3000 * (Math.random() - 0.5);
+		vertices.push(x, y, z);
+	}
+	const geometry = new THREE.BufferGeometry();
+	geometry.setAttribute(
+		"position",
+		new THREE.Float32BufferAttribute(vertices, 3),
+	);
+	const material = new THREE.PointsMaterial({
+		size: 10,
+		color: 0xffffff,
+	});
+	const mesh = new THREE.Points(geometry, material);
+	scene.add(mesh);
+}
+
+createStarField();
+
+tick();
+
+function tick() {
+	const targetRot = (mouseX / window.innerWidth) * 360;
+	rot += (targetRot - rot) * 0.02;
+	const radian = (rot * Math.PI) / 180;
+
+	camera.position.x = 1000 * Math.sin(radian);
+	camera.position.z = 1000 * Math.cos(radian);
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	renderer.render(scene, camera);
+
+	requestAnimationFrame(tick);
+}
