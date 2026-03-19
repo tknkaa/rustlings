111a63afb2e813193f8ed63a9d5b4bd3b5dccd00
tknkaa <tknkaa@users.noreply.github.com>
2026-03-19 11:56:59 +0800

    chore: complete options1 and quiz2 exercises

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,16 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(v, cmd)| match cmd {
+                Command::Uppercase => v.to_uppercase(),
+                Command::Trim => v.trim().to_string(),
+                Command::Append(n) => v + &("bar".repeat(n)),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -39,6 +49,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
